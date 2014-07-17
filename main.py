d09cb43a41b183accf2e2788a0cd1e1bc26111d8
Jonathan Leake <jleake527@gmail.com>
2014-07-17 16:45:14 -0500

    Added run() for testing purposes.

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
-
-
 @app.route('/')
 def hello():
     return render_template('example.html')
-
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+# For testing purposes.
+if __name__ == '__main__':
+    app.run()
